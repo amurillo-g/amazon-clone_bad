0c86e0a8e9c65dace45f538d974757c66004a6ae
Alex Murillo <amurillo_g@hotmail.com>
2023-03-12 14:57:36 -0600

    cambio a git cambio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Ahora
+          Git Cambio
         </a>
       </header>
     </div>
